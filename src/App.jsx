2ee5a8d3e10dea565fa05696dd985140bf1a02b2
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-03-24 04:44:44 +0600

    added reusable form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 function App() {
 
+  const handleSignUpSubmit = data =>{
+   console.log('sign up data', data)
+}
+
+  const handleUpdateSubmit = data =>{
+    console.log('update data', data);
+  }
 
   return (
     <>
@@ -13,7 +21,20 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={'Sign Up'} handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sign Up</h2>
+          <p>please sign up right now</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm formTitle={'Profile Update'} handleSubmit={handleUpdateSubmit} submitBtnText='Update'>
+        <div>
+          <h2>Update</h2>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReusableForm>
+      
       
     </>
   )
